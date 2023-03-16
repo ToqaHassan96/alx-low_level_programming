89a46999b79e2f8b0e02b7ac8f8c43efe82e0ea9
toqa <tokahassan40@gmail.com>
2023-03-16 16:59:37 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>  
-
 /**
-* main - Entry point
+ * main - A progam that prints with printf function.
 *
-* Description: A C progam that prints with printf function.
-*
-* Return: Always 0 (Success)
-*/
-
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
