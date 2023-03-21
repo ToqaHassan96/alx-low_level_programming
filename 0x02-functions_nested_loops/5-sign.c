3da6d239b89810eb10a7d61d2f6db7f8ad94b3f3
toqa <tokahassan40@gmail.com>
2023-03-21 16:31:59 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,15 +2,15 @@
 
 /**
  * print_sign - print + if n is greater than zero.
- * - if n is zero and - if n is less
- *   than zero
+ * 0 if n is zero and - if n is less
+ *   than zero.
  *
- *   on: takes integer types input for function.
+ *  @n: takes integer types input for function.
  *
  *   Return: 1 if +, 0 if 0 and -1 if -
  */
 
-int print_sing(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
