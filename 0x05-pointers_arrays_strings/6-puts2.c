f1bf4c79dbd19be5388147486907753ae263b4c8
toqa <tokahassan40@gmail.com>
2023-03-27 13:22:22 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -17,15 +17,13 @@ void puts2(char *str)
 		y++;
 		longi++;
 	}
-	t = longi++;
-}
-t = longi - 1;
-for (o = 0 ; o <= t ; o++)
-{
-	if (o % 2 == 0)
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
 	{
 		_putchar(str[0]);
 	}
-}
-_putchar('\n');
+	}
+	_putchar('\n');
 }
